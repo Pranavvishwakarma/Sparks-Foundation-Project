08d82384803bd1ff89d88f4b3dddc51d803d1654
Pranav Vishwakarma <103947396+Pranavvishwakarma@users.noreply.github.com>
2022-09-20 08:03:29 +0530

    Add files via upload

---
+++ Sparks-Foundation-Project-main/css/script.js
@@ -0,0 +1,26 @@
+let myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText);
+
+function sendMoney() {
+   var enterName = document.getElementById("enterName").value;
+   var enterAmount = parseInt(document.getElementById("enterAmount").value);
+
+   if (enterAmount > myAccountBalance) {
+      alert("Insufficient Balance.")
+   } else {
+      var findUserBankAccount = enterName + "BankBalance";
+      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+      var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
+      document.getElementById("myAccountBalance").innerText = myAccountBalance
+      document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+      alert(` Transaction Successful !!  
+      ₹${enterAmount} is sent to ${enterName}@email.com.`)
+
+      // transaction history 
+      var createPTag = document.createElement("li");
+      var textNode = document.createTextNode(`$${enterAmount} is sent to recepient with Email-id ${enterName}@email.com on ${Date()}.`);
+      createPTag.appendChild(textNode);
+      var element = document.getElementById("transaction-history-body");
+      element.insertBefore(createPTag, element.firstChild);
+   }
+}
+
